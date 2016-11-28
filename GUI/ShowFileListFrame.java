ab62e811f0532781393f78d6a83a7af579d16ef0
binglei5367 <15327700930m0@sina.cn>
2016-11-28 11:34:06 +0800

    preparing for sql

---
+++ GUI/ShowFileListFrame.java
@@ -0,0 +1,78 @@
+package GUI;
+
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Scrollbar;
+import javax.swing.JScrollPane;
+
+public class ShowFileListFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ShowFileListFrame frame = new ShowFileListFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ShowFileListFrame() {
+		
+		docList = new ArrayList<Doc>;
+		int rowNum = docList.size();
+		String[] tableHeadString = {"文件编号", "文件名", "描述", "上传者", "上传时间"};
+		String[][] tableData = new String[rowNum][5];
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane);
+		
+		table = new JTable();
+		panel.add(table);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		
+		JButton btnNewButton = new JButton("上传");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel_1.add(btnNewButton);
+		
+		JButton button = new JButton("\u4E0B\u8F7D");
+		panel_1.add(button);
+	}
+
+}
